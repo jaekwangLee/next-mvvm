543b0af59c09fa86028a18f507a553270935546e
jklee <jklee@jkleeui-MacBookAir.local>
2022-12-24 22:07:36 +0900

    FIX: (SEO) 수정

--- src/pages/wedding/[id].tsx
+++ src/pages/wedding/[id].tsx
@@ -8,12 +8,14 @@ import { useWedding } from '@components/template/template.viewModel';
 
 function WeddingJkjyPage(props: { id: string }) {
     const { info, reviewInfo } = useWedding(props.id);
-    const title = info
-        ? `모바일 청첩장 도착: 신랑 ${info?.groom.name}, 신부 ${info?.bride.name}의 결혼식에 당신을 초대합니다`
-        : '';
-    const desc = info
-        ? `신랑 ${info?.groom.name}, 신부 ${info?.bride.name}의 결혼식에 참석하여 축복해주세요. 신랑, 신부님이 당신의 축복을 진심으로 기다리고있습니다.`
-        : '';
+    const title = `모바일 청첩장 도착: 신랑 이재광, 신부 서지예의 결혼식에 당신을 초대합니다`;
+    // info
+    // ? `모바일 청첩장 도착: 신랑 ${info?.groom.name}, 신부 ${info?.bride.name}의 결혼식에 당신을 초대합니다`
+    // : '';
+    const desc = `신랑 이재광, 신부 서지예의 결혼식에 참석하여 축복해주세요. 신랑, 신부님이 당신의 축복을 진심으로 기다리고있습니다.`;
+    // info
+    // ? `신랑 ${info?.groom.name}, 신부 ${info?.bride.name}의 결혼식에 참석하여 축복해주세요. 신랑, 신부님이 당신의 축복을 진심으로 기다리고있습니다.`
+    // : '';
 
     return (
         <>
@@ -22,12 +24,12 @@ function WeddingJkjyPage(props: { id: string }) {
                 description={desc}
                 openGraph={{
                     type: 'website',
-                    url: `https://self-made.cloud/wedding/${props.id}`,
+                    url: `https://self-made.cloud/wedding/jkjy`, //-`https://self-made.cloud/wedding/${props.id}`,
                     title: title,
                     description: desc,
                     images: [
                         {
-                            url: info?.mainPhoto || '',
+                            url: 'https://wedding-cloud.s3.ap-northeast-2.amazonaws.com/IMG_0742.jpg', //- info?.mainPhoto || '',
                             width: 800,
                             height: 400
                         }
